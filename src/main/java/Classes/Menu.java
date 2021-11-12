d5ca9bdfd6e2527d52264e8c1754ecded377e5e8
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-11-11 19:20:48 -0500

    Creación de menú principal

---
+++ src/main/java/Classes/Menu.java
@@ -0,0 +1,58 @@
+package Classes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Menu {
+
+    public void iniciar(){
+        Scanner sc = new Scanner(System.in);
+        InputStreamReader i = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(i);
+
+
+        //Instancia de clase
+        Traductor tr = new Traductor();
+
+        //Variables globales
+        boolean exit = true;
+        int selection;
+        String esp, ingl, searchWord;
+
+        try{
+            do{
+                System.out.println("Bienvenido al traductor Español-Inglés");
+                System.out.println("1. Añadir palabras\n"+ "2. Buscar palabras\n"+ "3. Leer todo el diccionario\n"+ "4. " + "Salir\n");
+
+                selection = sc.nextInt();
+                switch (selection){
+                    case 1:
+                        System.out.println("Añada una palabra en español y su traducción");
+                        esp = bf.readLine();
+                        ingl = bf.readLine();
+                        tr.addWord(esp, ingl);
+                        break;
+                    case 2:
+                        System.out.println("Ingrese una palabra para su búsqueda");
+                        searchWord = bf.readLine();
+                        tr.searchWord(searchWord);
+                        break;
+                    case 3:
+                        tr.showDict();
+                        break;
+                    case 4:
+                        exit = false;
+                        break;
+
+                }
+            } while (exit);
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
