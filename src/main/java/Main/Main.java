4e314139ac93d9ebb8e539eab8c6f4251379959b
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-11-11 19:23:02 -0500

    Creación de instancia principal

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,11 +1,17 @@
 package Main;
 
 import Classes.Lecture;
+import Classes.Menu;
+import Classes.Traductor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
 
-        Lecture t = new Lecture();
-        t.leer("./Textos/archivo.txt");
+        Menu m1 = new Menu();
+        m1.iniciar();
     }
 }
