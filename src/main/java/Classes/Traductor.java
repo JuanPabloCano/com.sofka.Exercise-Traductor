9cb79bb40c2a068734fa8d60f587c43aef3120ac
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-11-11 19:21:57 -0500

    Creación de métodos para lectura y búsqueda de palabras en el diccionario

--- src/main/java/Classes/Traductor.java
+++ src/main/java/Classes/Traductor.java
@@ -1,20 +1,40 @@
 package Classes;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class Traductor {
 
-    private String spanishWord;
-    private String englishWord;
-    private HashMap <String, String> dict = new HashMap<>();
+    // Atributos encapsulados
+    private String spanishWord, englishWord, searchWord;
+    private Map <String, String> dict = new HashMap<>();
 
 
+    // Método para añadir palabras al diccionario
     public void addWord (String spanishWord, String englishWord){
         this.spanishWord = spanishWord;
         this.englishWord = englishWord;
         this.dict.put(spanishWord, englishWord);
     }
 
+    //Método para imprimir todo el diccionario usando la expresión Lambda
+    public Map <String, String> showDict(){
+
+        dict.forEach((k,v) -> System.out.println(k +" = "+ v));
+        return dict;
+    }
+
+    //Método para buscar palabras dentro del diccionario
+    public Map<String, String> searchWord(String searchWord){
+        if (dict.containsKey(searchWord)){
+            System.out.println("Su traducción es: "+ dict.get(searchWord));
+        }else {
+            System.out.println(searchWord + " no se encuentra en el diccionario");
+        }
+        return dict;
+    }
+
+
+    //
     public String getSpanishWord() {
         return spanishWord;
     }
@@ -31,11 +51,19 @@ public class Traductor {
         this.englishWord = englishWord;
     }
 
-    public HashMap<String, String> getDict() {
+    public String getSearchWord(){
+        return searchWord;
+    }
+
+    public void setSearchWord(String searchWord){
+        this.searchWord = searchWord;
+    }
+
+    public Map<String, String> getDict() {
         return dict;
     }
 
-    public void setDict(HashMap<String, String> dict) {
+    public void setDict(Map<String, String> dict) {
         this.dict = dict;
     }
 }
